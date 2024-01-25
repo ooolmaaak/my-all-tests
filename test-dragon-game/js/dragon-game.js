27dabe027f30b2d78381fa0579a43096f96c18fa
ooolmaaak <131387966+ooolmaaak@users.noreply.github.com>
2024-01-25 21:07:30 +0200

    add a new functions
    added a store weapons buying
    change the text
    added a new sellWeapon func

--- test-dragon-game/js/dragon-game.js
+++ test-dragon-game/js/dragon-game.js
@@ -4,6 +4,7 @@ let gold = 50;
 let currentWeapon = 0;
 let fighting;
 let monsterHealth;
+
 let inventory = ["stick"];
 
 const button1 = document.querySelector("#button1");
@@ -16,6 +17,25 @@ const goldText = document.querySelector("#goldText");
 const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
+
+const weapons = [
+  {
+    name: "stick",
+    power: 5,
+  },
+  {
+    name: "dagger",
+    power: 30,
+  },
+  {
+    name: "claw hammer",
+    power: 50,
+  },
+  {
+    name: "sword",
+    power: 100,
+  },
+];
 
 const locations = [
   {
@@ -69,16 +89,42 @@ function goStore() {
 }
 
 function goCave() {
-  console.log("Going to cave.");
+  update(locations[2]);
 }
 
 function fightDragon() {
   console.log("Fighting dragon.");
 }
 
-function buyHealth() {}
+function buyHealth() {
+  if (gold >= 10) {
+    goldText.innerText = gold -= 10;
+    healthText.innerText = health += 10;
+  } else {
+    text.innerText = "You do not have enough gold to buy health.";
+  }
+}
 
-function buyWeapon() {}
+function buyWeapon() {
+  if (currentWeapon < weapons.length - 1) {
+    if (gold >= 30) {
+      goldText.innerText = gold -= 30;
+      currentWeapon++;
+      let newWeapon = weapons[currentWeapon.name];
+      text.innerText = "You now have a " + newWeapon + ".";
+      inventory.push(newWeapon);
+      text.innerText += " In your inventory you have: " + inventory;
+    } else {
+      text.innerText = "You do not have enough gold to buy a weapon.";
+    }
+  } else {
+    text.innerText = "You already have the most powerful weapon!";
+    button2.innerText = "Sell weapon for 15 gold";
+    button2.onclick = sellWeapon;
+  }
+}
+
+function sellWeapon() {}
 
 function fightSlime() {}
 
