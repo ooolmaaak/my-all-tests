c1ae9be5852f8ae38f619c47bc0b2e5c75c1f221
ooolmaaak <131387966+ooolmaaak@users.noreply.github.com>
2024-01-28 11:21:51 +0200

    add comments

--- test-colorpicker/colorpicker.js
+++ test-colorpicker/colorpicker.js
@@ -1,3 +1,4 @@
+// получаем список цветов (массив объектов)
 const colors = [
   { hex: "#f44336", rgb: "244,67,54" },
   { hex: "#e91e63", rgb: "233,30,99" },
@@ -14,18 +15,27 @@ const colors = [
   { hex: "#607d8b", rgb: "96,125,139" },
 ];
 
+//получаем доступ к основному блоку, куда будем размещать цвета
 const paletteContainer = document.querySelector(".js-palette");
+//переменная для хранения HTML разметки новых созданных блоков
 const cardsMarkup = createColorCardsMarkup(colors);
 
+// вставляем HTML разметку перед окончанием основного блока
 paletteContainer.insertAdjacentHTML("beforeend", cardsMarkup);
 
+//добавляем слушателя события "клик" на каждый конкретный блок
+//принимаем параметр - функцию
 paletteContainer.addEventListener("click", onPaletteContainerClick);
 
+//переносим из HTML код для блоков сюда,
+//т.к будем перебирать коллекцию и создавать новые элементы на основании этой разметки
 function createColorCardsMarkup(colors) {
   return colors
     .map(({ hex, rgb }) => {
+      //деструктуризируем hex, rgb
       return `
-    <div class="color-card">
+  
+    <div class="color-card"> 
      <div><div><div> <div
      class="color-swatch"
      data-hex="${hex}"
@@ -42,14 +52,21 @@ function createColorCardsMarkup(colors) {
     .join("");
 }
 
+//основная функция, прнимающая другие функции
+//
 function onPaletteContainerClick(evt) {
+  //выбранный блок содержит класс color-swatch?
   const isColorSwatchEl = evt.target.classList.contains("color-swatch");
 
+  //если нет (!true) - ничего не происходит
   if (!isColorSwatchEl) {
     return;
   }
 
+  //если всё ОК - тогда:
+  //переменная для хранения текущего элемента
   const swatchEl = evt.target;
+  //переменная для хранения ближайшего родительского элемента с color-card
   const parentColorCard = swatchEl.closest(".color-card");
 
   removeActiveCardClass();
@@ -57,10 +74,12 @@ function onPaletteContainerClick(evt) {
   setBodyBgColor(swatchEl.dataset.hex);
 }
 
+// устанавливаем цвет фона в зависимости от принятого цвета в параметре
 function setBodyBgColor(color) {
   document.body.style.backgroundColor = color;
 }
 
+//функция, которая удаляет класс is-active с текущего блока
 function removeActiveCardClass() {
   const currentActiveCard = document.querySelector(".color-card.is-active");
 
@@ -69,6 +88,7 @@ function removeActiveCardClass() {
   }
 }
 
+//добавляем к текещему новому блоку с классом color-card - доп.класс is-active
 function addActiveCardClass(card) {
   card.classList.add("is-active");
 }
